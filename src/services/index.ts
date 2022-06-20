b8259c8423251e1f6cee4176364fd36cda9ce0d8
성해호 <heyho9292@gmail.com>
2022-06-20 23:23:18 +0900

    ~ env problem

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,4 @@
 export { default as AuthService } from "./auth.service";
 export { default as UserService } from "./user.service";
+export { default as OrderService } from "./order.service";
+export { default as ItemService } from "./item.service";
